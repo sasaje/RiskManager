34fbf37c95909f55a1eff1811b7d493dd94daee0
Louise Fuglsang Mortensen <70880930+LouiseFuglsang@users.noreply.github.com>
2020-12-16 10:12:04 +0100

    Archive added

--- src/risk/Archive.java
+++ src/risk/Archive.java
@@ -8,6 +8,15 @@
 
 package risk;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Archive {
 
     /* save()
@@ -20,4 +29,66 @@ public class Archive {
             deleteRisk
 
      */
+
+    void save(ActionEvent event) {
+
+        try {
+            Scanner input = new Scanner(System.in);
+            FileWriter myWriter = new FileWriter("archive.txt", true);
+            myWriter.write("" + riskAnalysis.getText() + '\n');
+
+            myWriter.close();
+            System.out.println("Der er skrevet i filen");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    void load(ActionEvent event) {
+
+        try {
+            File myObj = new File("archive.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String archiveData = myReader.nextLine();
+
+                // split the line on your splitter(s)
+                String[] splitted = archiveData.split(",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)"); // set , as delimiter
+
+                String riskAnalysisTitleFelt = splitted[0];
+                riskAnalysisTitle.setText(riskAnalysisTitleFelt);
+
+                String riskTitleFelt = splitted[1];
+                riskTitle.setText(riskTitleFelt);
+
+                String probabilityFelt = splitted[2];
+                probability.setText(probabilityFelt);
+
+                String consequenceFelt = splitted[3];
+                consequence.setText(consequenceFelt);
+
+                String priorityFelt = splitted[4];
+                priority.setText(priorityFelt);
+
+                String responseStrategyFelt = splitted[5];
+                responseStrategy.setText(responseStrategyFelt);
+
+                String revisedConsequenceFelt = splitted[6];
+                revisedConsequence.setText(revisedConsequenceFelt);
+
+                String revisedPriorityFelt = splitted[7];
+                revisedPriority.setText(revisedPriorityFelt);
+
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+/*
 }
